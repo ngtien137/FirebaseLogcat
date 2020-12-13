d6ecb26712c95955b7b91d37936b7b4281fc4f23
Nguyễn Văn Tiến <ngtien137@gmail.com>
2020-12-13 16:52:51 +0700

    fix default tag

--- app/src/main/java/com/lhd/demo/firebaselogcat/fb_logcat/LogEvent.kt
+++ app/src/main/java/com/lhd/demo/firebaselogcat/fb_logcat/LogEvent.kt
@@ -1,7 +1,7 @@
 package com.lhd.demo.firebaselogcat.fb_logcat
 
 class LogEvent(
-    private var message: Any?, val tag: String = "DefaultEvent"
+    private var message: Any?, val tag: String = "DefaultTAG"
 ) {
 
     fun getMessage() = message?.toString() ?: "[null]"
